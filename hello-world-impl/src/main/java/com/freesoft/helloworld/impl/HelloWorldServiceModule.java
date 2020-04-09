59ec1746f94f7ce7161dff5a44a700aa4f13ea93
dinabogdan <bogdan.dina03@gmail.com>
2020-04-09 21:55:59 +0300

    add DI

---
+++ hello-world-impl/src/main/java/com/freesoft/helloworld/impl/HelloWorldServiceModule.java
@@ -0,0 +1,15 @@
+package com.freesoft.helloworld.impl;
+
+import com.freesoft.helloworld.HelloWorldService;
+import com.google.inject.AbstractModule;
+import com.lightbend.lagom.javadsl.server.ServiceGuiceSupport;
+
+public class HelloWorldServiceModule
+        extends AbstractModule
+        implements ServiceGuiceSupport {
+
+    @Override
+    protected void configure() {
+        bindService(HelloWorldService.class, HelloWorldServiceImpl.class);
+    }
+}
--- hello-world-impl/src/test/java/com/freesoft/helloworld/impl/HelloWorldServiceImplTest.java
+++ hello-world-impl/src/test/java/com/freesoft/helloworld/impl/HelloWorldServiceImplTest.java
@@ -1,23 +1,31 @@
 package com.freesoft.helloworld.impl;
 
 import com.freesoft.helloworld.HelloWorldService;
+import com.lightbend.lagom.javadsl.testkit.ServiceTest;
 import org.junit.Test;
 
 import java.util.concurrent.ExecutionException;
 
+import static com.lightbend.lagom.javadsl.testkit.ServiceTest.*;
+import static com.lightbend.lagom.javadsl.testkit.ServiceTest.defaultSetup;
+import static com.lightbend.lagom.javadsl.testkit.ServiceTest.withServer;
 import static org.junit.Assert.assertEquals;
 
 public class HelloWorldServiceImplTest {
 
     @Test
-    public void testHelloWorldService() throws ExecutionException, InterruptedException {
-        HelloWorldService helloWorldService = new HelloWorldServiceImpl();
+    public void testHelloWorldService() {
+        withServer(defaultSetup(), server -> {
+                    HelloWorldService helloWorldService = server.client(HelloWorldService.class);
 
-        String result = helloWorldService.helloWorld()
-                .invoke()
-                .toCompletableFuture()
-                .get();
+                    String result = helloWorldService.helloWorld()
+                            .invoke()
+                            .toCompletableFuture()
+                            .get();
 
-        assertEquals("Hello World!", result);
+                    assertEquals("Hello World!", result);
+                }
+        );
+
     }
 }
